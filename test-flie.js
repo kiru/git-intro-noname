7b84ecec176a13f5554aa59c0a70eb6647a2be8e
Kirusanth Poopalasingam <code@kiru.io>
2021-05-26 16:23:34 +0200

    updated text

--- test-flie.js
+++ test-flie.js
@@ -17,8 +17,6 @@
  * limitations under the License.
  */
 
-// from Kiru: import .. 'test-file.js'
-
 import EventEmitter from 'events';
 import Log from '../utils/logger.js';
 import Browser from '../utils/browser.js';
@@ -34,7 +32,7 @@ import {InvalidArgumentException, IllegalStateException} from '../utils/exceptio
 class FlvPlayer {
 
     constructor(mediaDataSource, config) {
-        this.TAG = this.constructor.name;
+        this.TAG = 'FlvPlayer';
         this._type = 'FlvPlayer';
         this._emitter = new EventEmitter();
 
@@ -68,10 +66,7 @@ class FlvPlayer {
         this._pendingSeekTime = null;  // in seconds
         this._requestSetTime = false;
         this._seekpointRecord = null;
-        this._statisticsReporter = null;
         this._progressChecker = null;
-
-        this._hasStatisticsListener = false;
 
         this._mediaDataSource = mediaDataSource;
         this._mediaElement = null;
@@ -121,29 +116,16 @@ class FlvPlayer {
                 });
             }
         } else if (event === PlayerEvents.STATISTICS_INFO) {
-            this._hasStatisticsListener = true;
             if (this._statisticsInfo != null) {
                 Promise.resolve().then(() => {
                     this._emitter.emit(PlayerEvents.STATISTICS_INFO, this.statisticsInfo);
                 });
-            }
-            if (this._statisticsReporter == null) {
-                this._statisticsReporter = window.setInterval(
-                    this._reportStatisticsInfo.bind(this),
-                this._config.statisticsInfoReportInterval);
             }
         }
         this._emitter.addListener(event, listener);
     }
 
     off(event, listener) {
-        if (event === PlayerEvents.STATISTICS_INFO) {
-            this._hasStatisticsListener = false;
-            if (this._statisticsReporter != null) {
-                window.clearInterval(this._statisticsReporter);
-                this._statisticsReporter = null;
-            }
-        }
         this._emitter.removeListener(event, listener);
     }
 
@@ -155,7 +137,7 @@ class FlvPlayer {
         mediaElement.addEventListener('stalled', this.e.onvStalled);
         mediaElement.addEventListener('progress', this.e.onvProgress);
 
-        this._msectl = new MSEController();
+        this._msectl = new MSEController(this._config);
 
         this._msectl.on(MSEEvents.UPDATE_END, this._onmseUpdateEnd.bind(this));
         this._msectl.on(MSEEvents.BUFFER_FULL, this._onmseBufferFull.bind(this));
@@ -233,14 +215,6 @@ class FlvPlayer {
         this._transmuxer.on(TransmuxingEvents.MEDIA_SEGMENT, (type, ms) => {
             this._msectl.appendMediaSegment(ms);
 
-            if (this._hasStatisticsListener && this._statisticsReporter == null) {
-                if (this._progressChecker == null) {
-                    this._statisticsReporter = window.setInterval(
-                        this._reportStatisticsInfo.bind(this),
-                    this._config.statisticsInfoReportInterval);
-                }
-            }
-
             // lazyLoad check
             if (this._config.lazyLoad && !this._config.isLive) {
                 let currentTime = this._mediaElement.currentTime;
@@ -254,10 +228,6 @@ class FlvPlayer {
         });
         this._transmuxer.on(TransmuxingEvents.LOADING_COMPLETE, () => {
             this._msectl.endOfStream();
-            if (this._statisticsReporter != null) {
-                window.clearInterval(this._statisticsReporter);
-                this._statisticsReporter = null;
-            }
             this._emitter.emit(PlayerEvents.LOADING_COMPLETE);
         });
         this._transmuxer.on(TransmuxingEvents.RECOVERED_EARLY_EOF, () => {
@@ -271,10 +241,17 @@ class FlvPlayer {
         });
         this._transmuxer.on(TransmuxingEvents.MEDIA_INFO, (mediaInfo) => {
             this._mediaInfo = mediaInfo;
-            this._emitter.emit(PlayerEvents.MEDIA_INFO, mediaInfo);
+            this._emitter.emit(PlayerEvents.MEDIA_INFO, Object.assign({}, mediaInfo));
+        });
+        this._transmuxer.on(TransmuxingEvents.METADATA_ARRIVED, (metadata) => {
+            this._emitter.emit(PlayerEvents.METADATA_ARRIVED, metadata);
+        });
+        this._transmuxer.on(TransmuxingEvents.SCRIPTDATA_ARRIVED, (data) => {
+            this._emitter.emit(PlayerEvents.SCRIPTDATA_ARRIVED, data);
         });
         this._transmuxer.on(TransmuxingEvents.STATISTICS_INFO, (statInfo) => {
             this._statisticsInfo = this._fillStatisticsInfo(statInfo);
+            this._emitter.emit(PlayerEvents.STATISTICS_INFO, Object.assign({}, this._statisticsInfo));
         });
         this._transmuxer.on(TransmuxingEvents.RECOMMEND_SEEKPOINT, (milliseconds) => {
             if (this._mediaElement && !this._config.accurateSeek) {
@@ -287,10 +264,6 @@ class FlvPlayer {
     }
 
     unload() {
-        if (this._statisticsReporter != null) {
-            window.clearInterval(this._statisticsReporter);
-            this._statisticsReporter = null;
-        }
         if (this._mediaElement) {
             this._mediaElement.pause();
         }
@@ -305,7 +278,7 @@ class FlvPlayer {
     }
 
     play() {
-        this._mediaElement.play();
+        return this._mediaElement.play();
     }
 
     pause() {
@@ -370,6 +343,10 @@ class FlvPlayer {
     _fillStatisticsInfo(statInfo) {
         statInfo.playerType = this._type;
 
+        if (!(this._mediaElement instanceof HTMLVideoElement)) {
+            return statInfo;
+        }
+
         let hasQualityInfo = true;
         let decoded = 0;
         let dropped = 0;
@@ -391,12 +368,6 @@ class FlvPlayer {
         }
 
         return statInfo;
-    }
-
-    _reportStatisticsInfo() {
-        if (this._statisticsInfo != null) {
-            this._emitter.emit(PlayerEvents.STATISTICS_INFO, this.statisticsInfo);
-        }
     }
 
     _onmseUpdateEnd() {
@@ -436,11 +407,6 @@ class FlvPlayer {
         if (this._transmuxer) {
             this._transmuxer.pause();
 
-            if (this._statisticsReporter != null) {
-                window.clearInterval(this._statisticsReporter);
-                this._statisticsReporter = null;
-            }
-
             if (this._progressChecker == null) {
                 this._progressChecker = window.setInterval(this._checkProgressAndResume.bind(this), 1000);
             }
@@ -457,7 +423,7 @@ class FlvPlayer {
             let from = buffered.start(i);
             let to = buffered.end(i);
             if (currentTime >= from && currentTime < to) {
-                if (currentTime >= to - 30) {
+                if (currentTime >= to - this._config.lazyLoadRecoverDuration) {
                     needResume = true;
                 }
                 break;
